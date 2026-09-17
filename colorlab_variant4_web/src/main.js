6638918d43c41fea9e20a1b0bb27cb52f1821e47
Andrew58555 <nikon.andrew228@gmail.com>
2026-09-17 12:00:22 +0300

    Delete colorlab_variant4_web directory

--- colorlab_variant4_web/src/main.js
+++
@@ -1,7 +0,0 @@
-import { ColorConverter } from './model/converter.js';
-import { AppView } from './view/appView.js';
-import { ColorController } from './controller/colorController.js';
-
-const view = new AppView();
-const converter = new ColorConverter();
-new ColorController(view, converter);
